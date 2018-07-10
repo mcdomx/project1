1c29dcf85138756fa546f5c544b41d17d44c423d
Mark McDonald <mcdomx@me.com>
2018-07-10 19:34:58 -0400

    added environment variables shell script file

---
+++ env_variables.sh
@@ -0,0 +1,3 @@
+export FLASK_APP=application.py
+export FLASK_DEBUG=1
+export DATABASE_URL=postgres://vgltksvpsmnavf:5824aaa0eb811eb3aafefbdce73bd490e76ef577231e1b3632c23d64503f7539@ec2-107-20-193-202.compute-1.amazonaws.com:5432/d6avrg1hsn8aje
